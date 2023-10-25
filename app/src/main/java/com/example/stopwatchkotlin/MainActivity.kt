996b35f15795991c01b8387d0d5d439c8c1caf63
ylose <heungeob1003@gmail.com>
2023-10-25 13:47:51 +0900

    add event to button

--- app/src/main/java/com/example/stopwatchkotlin/MainActivity.kt
+++ app/src/main/java/com/example/stopwatchkotlin/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.stopwatchkotlin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btn_start: Button
+    private lateinit var btn_refresh: Button
+    private lateinit var tv_millisecond: TextView
+    private lateinit var tv_second: TextView
+    private lateinit var tv_minute: TextView
+
+    var isRunning = false
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start = findViewById(R.id.btn_start)
+        btn_refresh = findViewById(R.id.btn_refresh)
+        tv_millisecond = findViewById(R.id.tv_millisecond)
+        tv_second = findViewById(R.id.tv_second)
+        tv_minute = findViewById(R.id.tv_minute)
+
+        btn_start.setOnClickListener(this)
+        btn_refresh.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_start -> {
+                if (isRunning) {
+                    pause()
+                } else {
+                    start()
+                }
+            }
+
+            R.id.btn_refresh -> {
+                refresh()
+            }
+        }
+    }
+
+    // 스탑워치 시작
+    private fun start() {}
+    private fun pause() {}
+    private fun refresh() {}
+
 }
